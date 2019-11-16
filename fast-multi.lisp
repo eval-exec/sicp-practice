92e62780d190568eabd15142e7a5af60c196db8a
slarsar <slarsar@yandex.com>
2019-11-16 20:24:13 +0800

    fast-multi

---
+++ fast-multi.lisp
@@ -0,0 +1,22 @@
+
+(defun double (n)
+  (* n 2))
+
+(defun fast-multi (a b)
+  (cond
+   ((= b 2)
+    (double a))
+   ((evenp b)
+    (double (fast-multi a
+			(/ b 2))))
+   ((oddp b)
+    (+ a
+       (fast-multi a
+		   (- b 1))))))
+
+(setq )
+
+(fast-multi 9 9)
+(fast-multi 12 12)
+
+(fast-multi 9999999999999999999999876540 999999999)
